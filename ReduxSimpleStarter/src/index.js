92aef962f6143832db05510d0908b480a7be6a45
TedtheDev <tylerjedmunds@gmail.com>
2017-01-09 21:31:18 -0600

    added components and search_bar input

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyB4UgibWad5vXudv4h-uGcPInzNk_5u5Nc';
 
 //create a new component. THis component should produce some HTML
 const App = () => {
   return (
-    <div>Hi!</div>
+    <div>
+      <SearchBar />
+    </div>
   );
-}
+};
 
 //Take this component's generated HTML and put it on the page
 //(In the DOM)
