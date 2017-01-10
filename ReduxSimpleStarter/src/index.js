9a9f6f6ef52227170c38582dbbdc20b996743254
TedtheDev <tylerjedmunds@gmail.com>
2017-01-09 21:05:35 -0600

    first step - create hello world react app

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+//create a new component. THis component should produce some HTML
+const App = () => {
+  return (
+    <div>Hi!</div>
+  );
+}
+
+//Take this component's generated HTML and put it on the page
+//(In the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
