005c46bbdb28747bd78009b0fce85fa949168542
TedtheDev <tylerjedmunds@gmail.com>
2017-01-19 22:22:37 -0600

    added react-redux and connected reducer to book-list container

--- BookAppWithRedux/src/containers/book-list.js
+++ BookAppWithRedux/src/containers/book-list.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return(
         <li key={book.title} className='list-group-item'>{book.title}</li>
-      )
-    })
+      );
+    });
   }
 
   render() {
@@ -14,6 +15,15 @@ export default class BookList extends Component {
       <ul className='list-group col-sm-4'>
         {this.renderList()}
       </ul>
-    )
+    );
   }
 }
+
+function mapStateToProps(state) {
+  //whatever is returned will show up as props inside of BookList
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
