2f4b48809e04e004d0cc889404e01fff7a54f914
TedtheDev <tylerjedmunds@gmail.com>
2017-01-22 21:11:58 -0600

    added mapStateToProps on book-detail

--- BookAppWithRedux/src/reducers/index.js
+++ BookAppWithRedux/src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
-import ActiveBookReducer from './reduce_active_book';
+import ActiveBookReducer from './reducer_active_book';
 
 const rootReducer = combineReducers({
   books: BooksReducer,
