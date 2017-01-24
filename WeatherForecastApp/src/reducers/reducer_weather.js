ba3e7bbb8a9902eba665e68c533b4b07cd926542
TedtheDev <tylerjedmunds@gmail.com>
2017-01-23 23:27:02 -0600

    added switch statement and added action.payload.data to state array

--- WeatherForecastApp/src/reducers/reducer_weather.js
+++ WeatherForecastApp/src/reducers/reducer_weather.js
@@ -1,6 +1,14 @@
-export default function(state = null, action) {
-  console.log('Action recieved: ', action);
+import { FETCH_WEATHER } from '../actions/FETCH_WEATHER'
 
+export default function(state = [], action) {
+  switch(action.type) {
+    case FETCH_WEATHER:
+      //dont manipulate state with state.push(), use concat to create
+      //a new array
+      
+      //return state.concat([action.payload.data]); es6 below
+      return [action.payload.data, ...state];
+  }
 
   return state;
 }
