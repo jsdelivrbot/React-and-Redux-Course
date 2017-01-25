382d750631fd160c472680f7e72b7eed03528027
TedtheDev <tylerjedmunds@gmail.com>
2017-01-24 19:53:17 -0600

    added react-sparklines install; mapped temp data to temps; added sparklines chart with red color

--- WeatherForecastApp/src/containers/weather-list.js
+++ WeatherForecastApp/src/containers/weather-list.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
-
+    const temps = cityData.list.map(weather => weather.main.temp);
+    
     return(
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Sparklines height={60} width={90} data={temps}>
+            <SparklinesLine color='red' />
+          </Sparklines>
+        </td>
       </tr>
     )
   }
